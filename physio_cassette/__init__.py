bf7b14f60a9839906c7c017987972af3c91b6d13
LucaCerina <lccerina@gmail.com>
2024-06-25 11:23:27 +0200

    version 0.2.6

--- physio_cassette/__init__.py
+++ physio_cassette/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from .physio_cassette import *
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 __author__      = "Luca Cerina"
 __copyright__   = "Copyright 2022-2024, Luca Cerina"
 __email__       = "lccerina@duck.com"
